f9d432059da60551b70179bcf709d43a6911c837
Ruminiki Schmoeller <ruminiki@gmail.com>
2025-02-05 17:38:24 -0300

    .

--- document_processor.py
+++ document_processor.py
@@ -1,7 +1,7 @@
 # document_processor.py
 from abc import ABC, abstractmethod
 from typing import List
-import fitz
+from pypdf import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 from config import VectorDBConfig
@@ -26,9 +26,10 @@ class PDFProcessor(DocumentProcessor):
 
     def extract_text(self, pdf_path: str) -> str:
         text = ""
-        with fitz.open(pdf_path) as doc:
-            for page in doc:
-                text += page.get_text("text") + "\n"
+        reader = PdfReader(pdf_path)
+        
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
         return text
 
     def split_text(self, text: str) -> List[str]:
